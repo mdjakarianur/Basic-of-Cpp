79a182fad621aee98b916b9600204288ee64a79c
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2025-01-17 17:21:35 +0600

    Create To solve this problem.cpp

---
+++ To solve this problem.cpp
@@ -0,0 +1,5 @@
+/*
+To solve this problem, we can observe that the number of nodes at each depth follows a
+geometric progression with a common ratio of 3. Specifically, the number of nodes at depth
+can be calculated as 3^D.*/
+
