0e6ee7d2bf0833386a5699ecffb4805f4c1cb023
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2025-01-17 17:29:38 +0600

    Update To solve this problem.cpp

--- To solve this problem.cpp
+++ To solve this problem.cpp
@@ -3,3 +3,19 @@ To solve this problem, we can observe that the number of nodes at each depth fol
 geometric progression with a common ratio of 3. Specifically, the number of nodes at depth
 can be calculated as 3^D.*/
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+int N;
+cin>>N;
+if(N==0){
+    cout<<"1"<<endl;
+    return 0;
+}
+long long a1=3, r=3;
+long long totalNodes = (r*(pow(r,N)-1)/(r-1))+1;
+cout<<totalNodes <<endl;
+return 0;
+}
+
